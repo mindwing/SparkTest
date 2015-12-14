52984ea9599ccde81150873755024788b24bd842
Jase HA <mindwing@gmail.com>
2015-12-14 22:53:40 +0900

    add comment

--- WordCount/src/WordCount.scala
+++ WordCount/src/WordCount.scala
@@ -15,7 +15,7 @@ object WordCount {
     // 'bin/winutils.exe'
     System.setProperty("hadoop.home.dir", "c:\\Users\\mindwing\\warehouse\\")
 
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
     val sc = new SparkContext(conf)
 
     // various approaches.
@@ -29,9 +29,10 @@ object WordCount {
 
     println("fairy: " + fairy.count)
 
-    fairy.flatMap(x => x.split(" ")).map(x => (x, 1))
-      .reduceByKey((x, y) => (x + y))
-      .sortBy(_._2, false)
-      .take(10).map(println)
+    fairy.flatMap(x => x.split(" ")) // 빈 칸을 기준으로 낱말별로 나눔.
+      .map(x => (x, 1)) // (x, 1) 형태의 tuple 을 가지는 PairRDD 로 만듦.
+      .reduceByKey((x, y) => x + y) // 같은 key 를 가지는 value 중 두 개를 입력받아서 그 둘을 하나로 합침.
+      .sortBy(_._2, false) // tuple 중에서 _2 를 기준으로 역방향 정렬한다.
+      .take(10).map(println) // 순서상 맨 앞의 10개를 가져와서 println 한다.
   }
 }
